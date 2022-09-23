aa5c9210d0d188047968feb3af1915b0a47e4478
Ben Grinthal <bgrinthal@gmail.com>
2022-09-23 10:32:14 -0400

    added resolvers file, added some queries and mutations... in progress

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,6 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Exercise, Category, Order } = require('../models');
+const { User, Exercise, Category, Routine } = require('../models');
 const { signToken } = require('../utils/auth');
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
   Query: {
@@ -29,54 +28,29 @@ const resolvers = {
     user: async (parent, args, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id).populate({
-          path: 'orders.exercises',
+          path: 'routines.exercises',
           populate: 'category'
         });
 
-        user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+        user.routines.sort((a, b) => b.purchaseDate - a.purchaseDate);
 
         return user;
       }
 
       throw new AuthenticationError('Not logged in');
     },
-    order: async (parent, { _id }, context) => {
+    routine: async (parent, { _id }, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id).populate({
-          path: 'orders.exercises',
+          path: 'routines.exercises',
           populate: 'category'
         });
-        const order = new Order({ exercises: args.exercises });
+        const routine = new Routine({ exercises: args.exercises });
 
-        return user.orders.id(_id);
+        return user.routines.id(_id);
       }
 
       throw new AuthenticationError('Not logged in');
-    },
-    checkout: async (parent, args, context) => {
-      const url = new URL(context.headers.referer).origin;
-      const order = new Order({ exercises: args.exercises });
-      const line_items = [];
-
-      const { exercises } = await order.populate('exercises');
-
-      for (let i = 0; i < exercises.length; i++) {
-        const exercise = await stripe.exercises.create({
-          name: exercises[i].name,
-          description: exercises[i].description,
-          images: [`${url}/images/${exercises[i].image}`]
-        });
-      }
-
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items,
-        mode: 'payment',
-        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${url}/`
-      });
-
-      return { session: session.id };
     }
   },
   Mutation: {
@@ -86,14 +60,14 @@ const resolvers = {
 
       return { token, user };
     },
-    addOrder: async (parent, { exercises }, context) => {
+    addRoutine: async (parent, { exercises }, context) => {
       console.log(context);
       if (context.user) {
-        const order = new Order({ exercises });
+        const routine = new Routine({ exercises });
 
-        await User.findByIdAndUpdate(context.user._id, { $push: { orders: order } });
+        await User.findByIdAndUpdate(context.user._id, { $push: { routines: routine } });
 
-        return order;
+        return routine;
       }
 
       throw new AuthenticationError('Not logged in');
@@ -105,21 +79,21 @@ const resolvers = {
 
       throw new AuthenticationError('Not logged in');
     },
-    updateExercise: async (parent, { _id, quantity, reps, weight }) => {
-      const decrement = Math.abs(quantity) * -1;
+    // updateExercise: async (parent, { _id, quantity, reps, weight }) => {
+    //   const decrement = Math.abs(quantity) * -1;
 
-      //===========================================
-      const decrementReps = Math.abs(reps) * -1;
-      const decrementWeight = Math.abs(weight) * -1;
-      //===========================================
+    //   //===========================================
+    //   const decrementReps = Math.abs(reps) * -1;
+    //   const decrementWeight = Math.abs(weight) * -1;
+    //   //===========================================
 
-      return [await Exercise.findByIdAndUpdate(_id, { $inc: { quantity: decrement } }, { new: true }), //;
+    //   return [await Exercise.findByIdAndUpdate(_id, { $inc: { quantity: decrement } }, { new: true }), //;
 
-      //===========================================
-      Exercise.findByIdAndUpdate(_id, { $inc: { reps: decrementReps } }, { new: true }),
-      Exercise.findByIdAndUpdate(_id, { $inc: { weight: decrementWeight } }, { new: true })]
-      //===========================================
-    },
+    //   //===========================================
+    //   Exercise.findByIdAndUpdate(_id, { $inc: { reps: decrementReps } }, { new: true }),
+    //   Exercise.findByIdAndUpdate(_id, { $inc: { weight: decrementWeight } }, { new: true })]
+    //   //===========================================
+    // },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
