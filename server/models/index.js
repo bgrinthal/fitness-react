b6a7b0d4e29c852413cb20c907864e266ec214cc
Ben Grinthal <bgrinthal@gmail.com>
2022-10-03 10:21:34 -0400

    initial push

--- server/models/index.js
+++
@@ -1,6 +0,0 @@
-const User = require('./User');
-const Exercise = require('./Exercise');
-const Category = require('./Category');
-const Routine = require('./Routine');
-
-module.exports = { User, Exercise, Category, Routine };
