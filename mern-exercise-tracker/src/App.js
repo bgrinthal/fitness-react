f8c8f7390886b355a66df7f23b3e4229d74a8100
Ben Grinthal <bgrinthal@gmail.com>
2022-10-04 13:57:25 -0400

    pull

--- mern-exercise-tracker/src/App.js
+++ mern-exercise-tracker/src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // import components
-import Navbar from "./components/navbar"
-import ExerciseList from "./components/exercise-list"
-import EditExercise from "./components/edit-exercise"
-import CreateExercise from "./components/create-exercise"
-import CreateUser from "./components/create-user"
+import Navbar from "./components/navbar";
+import ExerciseList from "./components/exercise-list";
+import EditExercise from "./components/edit-exercise";
+import CreateExercise from "./components/create-exercise";
+import CreateUser from "./components/create-user";
 
 // react router adds url paths to component
 // Route / goes to ExerciseList, /edit/:id goes to EditExercise, ect.
 function App() {
+  
   return (
     <Router>
-      <Navbar />
-      <br />
-      <Route path="/" component={ExerciseList} />
-      <Route path="/edit/:id" component={EditExercise} />
-      <Route path="/create" component={CreateExercise} />
-      <Route path="/user" component={CreateUser} />
+      <div>
+        <Navbar />
+        <br />
+        <Route path="/" element={<ExerciseList />}/>
+        <Route path="/edit/:id" element={<EditExercise />} />
+        <Route path="/create" element={<CreateExercise />} />
+        <Route path="/user" element={<CreateUser />} />
+      </div>
     </Router>
   );
 }
