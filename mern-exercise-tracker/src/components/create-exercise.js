18c920c6ac9428793fffcde29055530c86ea1bee
Ben Grinthal <bgrinthal@gmail.com>
2022-10-11 17:37:46 -0400

    cleaned up by removing some unused code

--- mern-exercise-tracker/src/components/create-exercise.js
+++ mern-exercise-tracker/src/components/create-exercise.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
--- mern-exercise-tracker/src/components/create-user.js
+++ mern-exercise-tracker/src/components/create-user.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios'
 
 
