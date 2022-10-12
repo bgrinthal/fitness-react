2befa6371da6f478f31f33bcc34eab01592e183b
Ben Grinthal <bgrinthal@gmail.com>
2022-10-12 13:11:02 -0400

    removing redundant 'cart' code

--- mern-exercise-tracker/src/components/edit-exercise.js
+++ mern-exercise-tracker/src/components/edit-exercise.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 
-function EditExercise({ match }) {
+function EditExercise() {
     // states and set states
     const [username, setUsername] = useState('');
     const [photo, setPhoto] = useState('');
